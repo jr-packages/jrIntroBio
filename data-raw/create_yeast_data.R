ec45f25d053c5ac82caa3bc784609d3283788b38
Roman Popat <roman@datakitchen.co.uk>
2018-05-25 13:49:45 +0200

    edited 1-3 but not sure about 4

--- data-raw/create_yeast_data.R
+++ data-raw/create_yeast_data.R
@@ -1,6 +1,6 @@
 yeast = read.table("data-raw/yeast.data", header = FALSE,
                  col.names = c("seq", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "class"),
                  allowEscapes = TRUE)
-yeast_classes = yeast$class
-devtools::use_data(yeast)
-devtools::use_data(yeast_classes)
+yeast_classes = table(yeast$class)
+devtools::use_data(yeast, overwrite = TRUE)
+devtools::use_data(yeast_classes, overwrite = TRUE)
