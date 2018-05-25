77f83582d454dab817900331202a7ccba3b530af
Roman Popat <roman@datakitchen.co.uk>
2018-05-25 08:23:04 +0200

    added vector of yeast localisation classes

--- data-raw/create_yeast_data.R
+++ data-raw/create_yeast_data.R
@@ -1,4 +1,6 @@
 yeast = read.table("data-raw/yeast.data", header = FALSE,
                  col.names = c("seq", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "class"),
                  allowEscapes = TRUE)
+yeast_classes = yeast$class
 devtools::use_data(yeast)
+devtools::use_data(yeast_classes)
