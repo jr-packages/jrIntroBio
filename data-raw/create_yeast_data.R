925a878636f2abdf3c58b5d141be601c604c06f3
John McIntyre <john@jumpingrivers.com>
2019-12-12 17:17:48 +0000

    lintr checks

--- data-raw/create_yeast_data.R
+++ data-raw/create_yeast_data.R
@@ -2,7 +2,8 @@ library("tibble")
 library("dplyr")
 
 yeast = read.table("data-raw/yeast.data", header = FALSE,
-                 col.names = c("seq", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "class"),
+                 col.names = c("seq", "mcg", "gvh", "alm", "mit",
+                               "erl", "pox", "vac", "nuc", "class"),
                  allowEscapes = TRUE)
 yeast = as_tibble(yeast)
 
@@ -10,4 +11,3 @@ yeast_classes = yeast %>%
   count(class)
 saveRDS(yeast, file = "data/yeast.rds", version = 2)
 saveRDS(yeast_classes, file = "data/yeast_classes.rds", version = 2)
-
